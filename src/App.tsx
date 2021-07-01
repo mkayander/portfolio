9291163c83f30afda5650114aaecd92b98ecea1f
mkayander <maxim.kayander1@gmail.com>
2021-07-01 15:34:37 +0300

    Added initial styles & Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import {Header} from "./components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={styles.root}>
+            <Header/>
+            <main className="container">
+                <h3>Hello World</h3>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from "./Header.module.scss"
+import classNames from "classnames";
+
+const navigation = [
+    ["introduction", "Начало"],
+    ["about-me", "Обо мне"],
+    ["projects", "Мои работы"],
+    ["contacts", "Контакты"]
+]
+
+const Header = () => {
+    return (
+        <header className={classNames(styles.root, "container")}>
+            <nav>
+                <ul>
+                    {navigation.map(([id, name]) => (
+                        <li key={id} className={classNames({[styles.active]: id === "introduction"})}><a href={`#${id}`}>{name}</a></li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
