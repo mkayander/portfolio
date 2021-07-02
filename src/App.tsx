d2dde9b9e700b690e2004f31af0fc32f40040089
mkayander <maxim.kayander1@gmail.com>
2021-07-02 16:56:40 +0300

    Render introduction component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import styles from "./App.module.scss";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
-import {Header} from "./components";
+import {Header, Introduction} from "./components";
 
 function App() {
     return (
         <div className={styles.root}>
             <Header/>
-            <main className="container">
-                <h3>Hello World</h3>
+            <main>
+                <Introduction/>
+                <div className="container">
+                    <h3>Hello World</h3>
+                    <p>My name's Max</p>
+                    <br/>
+                </div>
             </main>
         </div>
     );
