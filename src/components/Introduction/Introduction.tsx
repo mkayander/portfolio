3b31f458da9949192a71a3894802ba83fe0dd164
mkayander <maxim.kayander1@gmail.com>
2021-07-05 16:57:26 +0300

    Improved introduction section. Added dynamic scroll-down button.

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import BgImg from "./assets/Bg_Image_Mask.svg";
 import Shape1 from "./assets/Shape_1.svg";
 import Shape2 from "./assets/Shape_2.svg";
+import ScrollIcon from "./assets/Scroll_Down_Button.svg";
 import styles from "./Introduction.module.scss";
 import classNames from "classnames";
 import { Button } from "../index";
+import { addWindowEvents, removeWindowEvents } from "../../utils/windowEvents";
 
-const Introduction = () => {
+enum ScrollButtonState {
+    fixed = "fixed",
+    relative = "relative",
+}
+
+const scrollButtonOffset = 160;
+
+const Introduction: React.FC = () => {
+    const bgRef = useRef<HTMLImageElement>(null);
+    const buttonScrollRef = useRef<HTMLButtonElement>(null);
+
+    const [scrollButtonState, setScrollButtonState] = useState(ScrollButtonState.fixed);
+
+    useEffect(() => {
+        const listener = () => {
+            if (bgRef?.current && buttonScrollRef?.current) {
+                if (window.getComputedStyle(buttonScrollRef.current).display === "none") return;
+
+                switch (scrollButtonState) {
+                    case ScrollButtonState.fixed:
+                        if (
+                            bgRef.current.height - scrollButtonOffset < buttonScrollRef.current.offsetTop ||
+                            bgRef.current.height - window.scrollY - scrollButtonOffset <
+                                buttonScrollRef.current.offsetTop
+                        ) {
+                            // console.log("Make button position: relative");
+                            setScrollButtonState(ScrollButtonState.relative);
+                        }
+
+                        break;
+
+                    case ScrollButtonState.relative:
+                        if (window.innerHeight > bgRef.current.height) return;
+                        if (window.innerHeight + window.scrollY < buttonScrollRef?.current?.offsetTop) {
+                            console.log(window.innerHeight);
+                            console.log(window.scrollY);
+                            console.log(buttonScrollRef?.current?.offsetTop);
+                            setScrollButtonState(ScrollButtonState.fixed);
+                            console.log("Make button fixed!");
+                        }
+                        break;
+                }
+            }
+        };
+
+        addWindowEvents(["scroll", "resize"], listener);
+        listener();
+
+        return () => {
+            removeWindowEvents(["scroll", "resize"], listener);
+        };
+    }, [scrollButtonState]);
+
     return (
         <section className={styles.root}>
             <div className={classNames(styles.content, "container")}>
@@ -28,7 +82,13 @@ const Introduction = () => {
             </div>
             <img className={styles.bgImage} src={BgImg} alt="Background" />
             <img className={styles.bgImage} src={Shape1} alt="Background" />
-            <img className={styles.bgMain} src={Shape2} alt="Background" />
+            <img ref={bgRef} className={styles.bgMain} src={Shape2} alt="Background" />
+            <button
+                ref={buttonScrollRef}
+                className={classNames(styles.buttonScroll, styles[scrollButtonState])}
+                onClick={event => console.log(event)}>
+                <img src={ScrollIcon} alt="Scroll Down" />
+            </button>
         </section>
     );
 };
