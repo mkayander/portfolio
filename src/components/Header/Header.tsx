e0facf1d1e13c5678097d2bf003246dbefdeb6e7
mkayander <maxim.kayander1@gmail.com>
2021-07-06 09:50:07 +0300

    Added 'scrolled' style to Header component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./Header.module.scss";
 import classNames from "classnames";
+import { addWindowEvents, removeWindowEvents } from "../../utils/windowEvents";
 
 const navigation = [
     ["introduction", "Начало"],
@@ -11,9 +12,28 @@ const navigation = [
 ];
 
 const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const listener = () => {
+            if (window.scrollY > 64) {
+                console.log(window.scrollY);
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        addWindowEvents(["load", "scroll", "resize"], listener);
+
+        return () => {
+            removeWindowEvents(["load", "scroll", "resize"], listener);
+        };
+    }, []);
+
     return (
-        <header className={classNames(styles.root, "container")}>
-            <nav>
+        <header className={classNames(styles.root, { [styles.scrolled]: isScrolled })}>
+            <nav className="container">
                 <ul>
                     {navigation.map(([id, name]) => (
                         <li key={id} className={classNames({ [styles.active]: id === "introduction" })}>
