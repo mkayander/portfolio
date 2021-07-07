6a0847b46bdbfae38aa25780f6a7e7166a1f2b76
mkayander <maxim.kayander1@gmail.com>
2021-07-07 10:02:04 +0300

    Implemented a safe way of parsing twemoji.

--- src/components/Twemoji/Twemoji.tsx
+++ src/components/Twemoji/Twemoji.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import twemoji from "twemoji";
+
+enum TwemojiMode {
+    parseNode,
+    innerHtml,
+}
 
 type TwemojiProps = {
     emoji: string;
+    mode?: TwemojiMode;
 };
 
-const Twemoji: React.FC<TwemojiProps> = ({ emoji }) => {
-    return <span dangerouslySetInnerHTML={{ __html: twemoji.parse(emoji, {}) }} />;
+const Twemoji: React.FC<TwemojiProps> = ({ emoji, mode = TwemojiMode.parseNode }) => {
+    const ref = useRef<HTMLSpanElement>(null);
+
+    useEffect(() => {
+        if (mode === TwemojiMode.parseNode && ref.current) {
+            ref.current && twemoji.parse(ref.current);
+        }
+    }, [mode]);
+
+    switch (mode) {
+        case TwemojiMode.innerHtml:
+            return <span dangerouslySetInnerHTML={{ __html: twemoji.parse(emoji) }} />;
+
+        default:
+        case TwemojiMode.parseNode:
+            return <span>{emoji}</span>;
+    }
 };
 
 export default React.memo(Twemoji);
