149bcec3c9d39ffbb4063f5bfff2b5e421580adc
Max Kayander <maxim.kayander1@gmail.com>
2021-07-09 16:23:52 +0300

    Added other contacts. Improved styles.

--- src/sections/ContactsSection/ContactsSection.tsx
+++ src/sections/ContactsSection/ContactsSection.tsx
@@ -4,6 +4,10 @@ import { Heading } from "../../components";
 import WhatsAppIcon from "./assets/Whatsapp_Icon_contact.svg";
 import GmailIcon from "./assets/Email_Icon_contact.svg";
 import TelegramIcon from "./assets/Telegram_Icon_contact.svg";
+import LinkedInIcon from "./assets/LinkedIn_Icon_contact.svg";
+import GithubIcon from "./assets/GitHub_Icon_contact.svg";
+import VKIcon from "./assets/VK_Icon_contact.svg";
+import classNames from "classnames";
 
 type Contact = {
     type: "mobile" | "email" | "url";
@@ -16,6 +20,14 @@ const contactsData: Contact[] = [
     { type: "mobile", iconUrl: WhatsAppIcon, title: "Мобильный", value: "+7 (977) 491-90-67" },
     { type: "email", iconUrl: GmailIcon, title: "Email", value: "maxim.kayander1@gmail.com" },
     { type: "url", iconUrl: TelegramIcon, title: "Telegram", value: "https://t.me/mkayander" },
+    {
+        type: "url",
+        iconUrl: LinkedInIcon,
+        title: "LinkedIn",
+        value: "https://www.linkedin.com/in/max-kayander-54a42b211/",
+    },
+    { type: "url", iconUrl: GithubIcon, title: "GitHub", value: "https://github.com/mkayander" },
+    { type: "url", iconUrl: VKIcon, title: "VK", value: "https://vk.com/mkayander" },
 ];
 
 const getContactHref = (contact: Contact) => {
@@ -39,9 +51,11 @@ const ContactsSection = () => {
                 <div className={styles.card}>
                     {contactsData.map(contact => (
                         <div className={styles.row}>
-                            <img src={contact.iconUrl} alt="Icon" />
-                            <h5>{contact.title}: </h5>
-                            <a href={getContactHref(contact)}>{contact.value}</a>
+                            <div className={classNames("col-3", styles.titleCol)}>
+                                <img src={contact.iconUrl} alt="Icon" />
+                                <h5>{contact.title}: </h5>
+                            </div>
+                            <a className={styles.valueCol} href={getContactHref(contact)}>{contact.value}</a>
                         </div>
                     ))}
                 </div>
