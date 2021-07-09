da8cf30fdff087d6a530d10caecba14f85447da4
Max Kayander <maxim.kayander1@gmail.com>
2021-07-09 16:23:29 +0300

    Implemented Footer component.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from "./Header/Header";
+export { default as Footer } from "./Footer/Footer";
 export { default as Button } from "./Button/Button";
 export { default as ScrollButton } from "./ScrollButton/ScrollButton";
 export { default as FoldableContent } from "./FoldableContent/FoldableContent";
