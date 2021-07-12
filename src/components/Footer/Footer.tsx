cadee794a7159d24a9264263e7b3ebb2d8f6db7e
Max Kayander <maxim.kayander1@gmail.com>
2021-07-12 17:03:30 +0300

    Accept default attrs (inline styles)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "./Footer.module.scss";
 import Shape from "./assets/Footer_Shape.svg";
 
-const Footer = () => {
+const Footer: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({...rest}) => {
     return (
-        <footer className={styles.root}>
+        <footer className={styles.root} {...rest}>
             <img className={styles.shape} src={Shape} alt="Shape" />
             <div className={styles.content}>
                 <h5>Спасибо за уделённое время!</h5>
