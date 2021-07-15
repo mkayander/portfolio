94a2fbd9155c1a7dd7bc530efa3a742b113a49b1
Max Kayander <maxim.kayander1@gmail.com>
2021-07-15 11:34:20 +0300

    Implemented smooth scroll to section on App component mount based on link hash.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./App.module.scss";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 import { Footer, Header } from "./components";
@@ -6,6 +6,7 @@ import { AboutMeSection, ContactsSection, Introduction, PortfolioSection } from 
 import { useSelector } from "react-redux";
 import { Section, selectSections } from "./reducers/sectionReducer";
 import landingNavigation from "./landingNavigation";
+import { scrollToSection } from "./utils/doScroll";
 
 function App() {
     const sectionsMap = useSelector(selectSections) as Record<keyof typeof landingNavigation, Section>;
@@ -16,6 +17,24 @@ function App() {
     ) => {
         return (props: T) => <Component id={id} ref={sectionsMap[id]?.ref} {...props} />;
     };
+
+    useEffect(() => {
+        let handler = () => {
+        };
+
+        if (window.location.hash) {
+            const id = window.location.hash.substring(1);
+            handler = () => scrollToSection(sectionsMap[id as keyof typeof landingNavigation]);
+            if (id in sectionsMap) {
+                window.addEventListener("load", handler);
+            }
+        }
+
+        return () => {
+            window.removeEventListener("load", handler);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <div className={styles.root}>
