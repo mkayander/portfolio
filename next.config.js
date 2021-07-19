9dc54250cf01306878e67d9e73aad59f04af4e9f
Max Kayander <maxim.kayander1@gmail.com>
2021-07-19 12:05:31 +0300

    Installed Next.JS & next-images . Initiated config files. #1

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+const withImages = require("next-images");
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+    /* config options here */
+};
+
+module.exports = withImages(nextConfig);
