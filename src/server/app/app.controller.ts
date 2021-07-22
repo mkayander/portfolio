fc2d4e0aedcd517c9cf13f24c3b3c1d3dbaf331a
Max Kayander <maxim.kayander1@gmail.com>
2021-07-22 16:10:19 +0300

    Implemented a working View module that uses NextJS for rendering pages. #3

--- src/server/app/app.controller.ts
+++ src/server/app/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Req } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { Request } from "express";
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
-    @Get()
+    @Get("/hello-world")
     getHello(): string {
         return this.appService.getHello();
+    }
+
+    @Get("/test")
+    getTest(@Req() request: Request): string {
+        return `Request data: ${request} ${request.cookies} ${request.query}`;
     }
 }
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,10 +1,11 @@
 // noinspection JSIgnoredPromiseFromCall
 
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app/app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { ServerModule } from "./server.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(ServerModule);
     await app.listen(3000);
 }
 
