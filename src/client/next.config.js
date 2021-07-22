f05cf16f13fec903c2222ad34bcfab15c568060b
Max Kayander <maxim.kayander1@gmail.com>
2021-07-22 11:53:45 +0300

    Moved clientside to a separate directory. #3

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -1,16 +1,12 @@
-// @ts-check
-const withImages = require("next-images");
+// const withImages = require("next-images");
 
-// @ts-check
 /**
- * @type {import('next').NextConfig}
+ // * @type {next.NextConfig}
  **/
 const nextConfig = {
-    /* config options here */
+    distDir: "../../.next",
     images: {
         domains: ["i.ibb.co"],
-        // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        // deviceSizes: [576, 768, 992, 1200, 1400, 1920, 2048, 3840],
     },
 
     webpack(config) {
