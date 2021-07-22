e041232683302278f99eadc394e17215f187b474
Max Kayander <maxim.kayander1@gmail.com>
2021-07-22 14:21:00 +0300

    Moved files

---
+++ src/client/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    extends: ["eslint-config-next"],
+    rules: {},
+    overrides: [
+        {
+            files: ["**/*.ts?(x)"],
+            rules: {},
+        },
+    ],
+};
