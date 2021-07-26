696b2c4e2590cf58213911af520b195fb6ca0616
Max Kayander <maxim.kayander1@gmail.com>
2021-07-26 15:27:45 +0300

    Added MySQL ORM config file.

---
+++ src/server/ormconfigMySQL.ts
@@ -0,0 +1,23 @@
+import { ConnectionOptions } from "typeorm";
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const connectionOptions: ConnectionOptions = {
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    database: process.env.MYSQL_DB_NAME,
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    entities: ["dist/**/entities/*.js"],
+    migrations: ["dist/migrations/*.js"],
+    cli: {
+        migrationsDir: "src/server/migrations",
+    },
+};
+
+export = connectionOptions;
+
+console.log(connectionOptions);
