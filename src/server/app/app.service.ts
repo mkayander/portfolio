874570193a5cb0661b6d8d0401b94d0ca04e0aca
Max Kayander <maxim.kayander1@gmail.com>
2021-07-26 16:15:09 +0300

    Log mapped typeORM entities to console on init.

--- src/server/app/app.service.ts
+++ src/server/app/app.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { InjectConnection } from "@nestjs/typeorm";
+import { Connection } from "typeorm";
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+    constructor(@InjectConnection() private readonly connection: Connection) {}
+
+    onModuleInit() {
+        console.log(this.connection.entityMetadatas.map(value => value.name));
+    }
+
     getHello(): string {
         return "Hello World!";
     }
