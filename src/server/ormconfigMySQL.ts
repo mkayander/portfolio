0eef09484583c2fed9c123e69d8f21abb03a27b2
Max Kayander <maxim.kayander1@gmail.com>
2021-07-26 16:42:49 +0300

    Disable synchronization explicitly for MySQL ORM config.

--- src/server/ormconfigMySQL.ts
+++ src/server/ormconfigMySQL.ts
@@ -8,6 +8,7 @@ const connectionOptions: ConnectionOptions = {
     type: "mysql",
     host: "localhost",
     port: 3306,
+    synchronize: false,
     database: process.env.MYSQL_DB_NAME,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
