dcb01f6f870a8acdf3392812ada01d3a8dab6c59
Max Kayander <maxim.kayander1@gmail.com>
2021-07-26 15:27:13 +0300

    Removed commented-out code.

--- src/server/ormconfigMongo.ts
+++ src/server/ormconfigMongo.ts
@@ -8,15 +8,10 @@ const connectionOptions: ConnectionOptions = {
     type: "mongodb",
     url: process.env.MONGODB_CONNECTION_URL || "mongodb://localhost:27017/",
     database: process.env.MONGODB_DATABASE,
-    // entities: [join(__dirname, "**", "entities", "*.entity{.ts,.js}")],
     entities: ["dist/**/entities/*.js"],
-    // entities: ["src/server/**/entities/*.entity.ts"],
     migrations: ["dist/migrations/*.js"],
-    // migrations: ["src/server/migrations/*.{ts,js}"],
-    // ssl: true
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    // migrations: [join(__dirname, "migrations/*{.ts,.js}")],
     cli: {
         migrationsDir: "src/server/migrations",
     },
