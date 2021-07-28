c5e43855c6409e417901e878d28508e7b8be3078
Max Kayander <maxim.kayander1@gmail.com>
2021-07-28 15:32:18 +0300

    Readable console log.

--- src/server/app/app.service.ts
+++ src/server/app/app.service.ts
@@ -7,7 +7,10 @@ export class AppService implements OnModuleInit {
     constructor(@InjectConnection() private readonly connection: Connection) {}
 
     onModuleInit() {
-        console.log(this.connection.entityMetadatas.map(value => value.name));
+        console.log(
+            "Mapped TypeORM entities: ",
+            this.connection.entityMetadatas.map(value => value.name)
+        );
     }
 
     getHello(): string {
