7769183621c8ff2e3f72e25e0bfd427434243b82
Max Kayander <maxim.kayander1@gmail.com>
2021-07-29 00:13:04 +0300

    Disable global exception filter.

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,11 +3,10 @@
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { ServerModule } from "./server.module";
-import { GlobalExceptionFilter } from "./app/filters/global-exception.filter";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(ServerModule);
-    app.useGlobalFilters(new GlobalExceptionFilter());
+    // app.useGlobalFilters(new GlobalExceptionFilter());
     await app.listen(3000);
 }
 
