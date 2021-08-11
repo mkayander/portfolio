8d3435176ec3b6bbe21b6f0e583197db469d6d76
Max Kayander <maxim.kayander1@gmail.com>
2021-08-11 20:53:40 +0300

    Provide router module for api routes nesting with prefix.

--- src/server/server.module.ts
+++ src/server/server.module.ts
@@ -1,8 +1,40 @@
 import { Module } from "@nestjs/common";
 import { AppModule } from "./app/app.module";
 import { ViewModule } from "./view/view.module";
+import { RouterModule } from "@nestjs/core";
+import { ProjectsModule } from "./projects/projects.module";
+import { ContactsModule } from "./contacts/contacts.module";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-    imports: [AppModule, ViewModule],
+    imports: [
+        AppModule,
+        RouterModule.register([
+            {
+                path: "api/v1",
+                module: AppModule,
+                children: [
+                    {
+                        path: "/",
+                        module: UsersModule,
+                    },
+                    {
+                        path: "/",
+                        module: ProjectsModule,
+                    },
+                    {
+                        path: "/",
+                        module: ContactsModule,
+                    },
+                    {
+                        path: "/",
+                        module: AuthModule,
+                    },
+                ],
+            },
+        ]),
+        ViewModule,
+    ],
 })
 export class ServerModule {}
