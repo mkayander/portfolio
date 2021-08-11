37b14a3fa4285a4b7d160eea4d2451c5724ed0fa
Max Kayander <maxim.kayander1@gmail.com>
2021-08-12 01:23:24 +0300

    Added global axios error handler.

--- src/client/api/index.ts
+++ src/client/api/index.ts
@@ -2,8 +2,16 @@ import axios from "axios";
 import { Project } from "./models";
 
 export const api = axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? "blablalbla" : "https://localhost:3000/api/",
+    baseURL: process.env.NODE_ENV === "production" ? "blablalbla" : "http://localhost:3000/api/v1/",
 });
+
+api.interceptors.response.use(
+    response => response,
+    error => {
+        console.error("Axios error!: ", error.message, error.config);
+        return Promise.reject(error);
+    }
+);
 
 export default api;
 
