298d72ad3a7560334cef6ee9657379db90896bc6
Max Kayander <maxim.kayander1@gmail.com>
2021-08-15 18:13:54 +0300

    Make API data readonly for guest users.

--- src/server/contacts/contacts.controller.ts
+++ src/server/contacts/contacts.controller.ts
@@ -1,21 +1,35 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    UploadedFile,
+    UseGuards,
+    UseInterceptors,
+} from "@nestjs/common";
 import { ContactsService } from "./contacts.service";
 import { CreateContactDto } from "./dto/create-contact.dto";
 import { UpdateContactDto } from "./dto/update-contact.dto";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { diskStorage } from "multer";
 import { editFileName } from "../projects/file-uploading.utils";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 
 @Controller("contacts")
 export class ContactsController {
     constructor(private readonly contactsService: ContactsService) {}
 
     @Post()
+    @UseGuards(JwtAuthGuard)
     create(@Body() createContactDto: CreateContactDto) {
         return this.contactsService.create(createContactDto);
     }
 
     @Post(":id/icon")
+    @UseGuards(JwtAuthGuard)
     @UseInterceptors(
         FileInterceptor("image", {
             storage: diskStorage({
@@ -41,11 +55,13 @@ export class ContactsController {
     }
 
     @Patch(":id")
+    @UseGuards(JwtAuthGuard)
     update(@Param("id") id: string, @Body() updateContactDto: UpdateContactDto) {
         return this.contactsService.update(+id, updateContactDto);
     }
 
     @Delete(":id")
+    @UseGuards(JwtAuthGuard)
     remove(@Param("id") id: string) {
         return this.contactsService.remove(+id);
     }
