da0040cfdb28b489db958de0be1e086eb983d92f
Max Kayander <maxim.kayander1@gmail.com>
2021-08-16 12:53:12 +0300

    Added temporary solution for generating a unique integer.

--- src/server/migrations/1628769764909-AddIndexColumn.ts
+++ src/server/migrations/1628769764909-AddIndexColumn.ts
@@ -4,8 +4,15 @@ export class AddIndexColumn1628769764909 implements MigrationInterface {
     name = "AddIndexColumn1628769764909";
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // SET SQL_SAFE_UPDATES = 0;
+        // ALTER TABLE `contact` ADD `index` int NOT NULL;
+        // UPDATE `contact` SET `index` = (FLOOR(RAND()*(100-10)+10)) WHERE `index` = 0;
+        // SET SQL_SAFE_UPDATES = 1;
+        await queryRunner.query("SET SQL_SAFE_UPDATES = 0");
         await queryRunner.query("ALTER TABLE `contact` ADD `index` int NOT NULL");
+        await queryRunner.query("UPDATE `contact` SET `index` = FLOOR(RAND()*(100-10)+10)");
         await queryRunner.query("ALTER TABLE `contact` ADD UNIQUE INDEX `IDX_01112e50b493dc0fdc588335ad` (`index`)");
+        await queryRunner.query("SET SQL_SAFE_UPDATES = 1");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
