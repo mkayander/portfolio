93da33fe7096aee64709b873805305698d7d95d7
Max Kayander <maxim.kayander1@gmail.com>
2021-08-18 13:55:42 +0300

    Installed 'readline-sync' to set up superuser password in a secure way.

--- src/server/commands/createSuperUser.ts
+++ src/server/commands/createSuperUser.ts
@@ -2,10 +2,11 @@ import { createConnection } from "typeorm";
 import * as options from "../ormconfigMySQL";
 import { User } from "../users/entities/user.entity";
 import { Role } from "../users/role.enum";
+import * as readlineSync from "readline-sync";
 
 console.log(process.argv.slice(2));
 
-const [email, password] = process.argv.slice(2);
+const [email] = process.argv.slice(2);
 
 const insertUser = async () => {
     const connection = await createConnection({ ...options, entities: [User] });
@@ -13,8 +14,9 @@ const insertUser = async () => {
     const repository = connection.getRepository(User);
 
     const user = repository.create();
-    user.email = email;
-    user.passwordRaw = password;
+    user.email = email || readlineSync.questionEMail("Enter E-Mail: ");
+
+    user.passwordRaw = readlineSync.questionNewPassword("Enter password: ", { min: 8 });
     user.roles = [Role.Admin];
 
     await user.save();
