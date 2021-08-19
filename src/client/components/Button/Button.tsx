64b828d8157bce4ce2f90c7c7c611f0ac99d1a06
Max Kayander <maxim.kayander1@gmail.com>
2021-08-19 16:19:05 +0300

    Render link buttons with '<a/>' tag. Provide 'new tab' option.

--- src/client/components/Button/Button.tsx
+++ src/client/components/Button/Button.tsx
@@ -6,17 +6,27 @@ import classNames from "classnames";
 type ButtonProps = {
     text?: string;
     color?: "accent" | "primary";
+    onClick?: React.MouseEventHandler;
+    link?: string;
+    // Adds 'target="_blank"' and 'rel="noreferrer"' attributes to anchor element if 'link' prop is specified.
+    openNewTab?: boolean;
 };
 
-const Button: React.FC<React.HTMLAttributes<HTMLButtonElement> & ButtonProps> = ({
-    text,
-    color = "accent",
-    ...rest
-}) => {
+/**
+ * If URL is passed to the 'link' argument, the button would be rendered as <a/> tag with specified href.
+ */
+const Button: React.FC<ButtonProps> = ({ text, color = "accent", onClick, link, openNewTab }) => {
+    const Tag = link === undefined ? "button" : "a";
+
     return (
-        <button className={classNames(styles.root, styles[color])} {...rest}>
+        <Tag
+            className={classNames(styles.root, styles[color])}
+            href={link}
+            target={link && openNewTab && "_blank"}
+            rel={link && openNewTab && "noreferrer"}
+            onClick={onClick}>
             {text && <span>{text}</span>}
-        </button>
+        </Tag>
     );
 };
 
