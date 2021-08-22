024ae9fc798ffed0bcf089f18343f08356b2abe6
Max Kayander <maxim.kayander1@gmail.com>
2021-08-22 17:22:03 +0300

    Implemented CV upload and URL getter logic.

--- src/server/app/app.controller.ts
+++ src/server/app/app.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get, Post, Req, StreamableFile, UseGuards } from "@nestjs/common";
+import {
+    Controller,
+    Get,
+    InternalServerErrorException,
+    Post,
+    Req,
+    Res,
+    UploadedFile,
+    UseGuards,
+    UseInterceptors,
+} from "@nestjs/common";
 import { AppService } from "./app.service";
 import { LocalAuthGuard } from "../auth/local-auth.guard";
-import { Request } from "express";
+import { Request, Response } from "express";
 import { AuthService } from "../auth/auth.service";
-import { createReadStream } from "fs";
+import * as fs from "fs";
 import { join } from "path";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { genericFileFilter } from "../utils/file-uploading.utils";
 
 @Controller()
 export class AppController {
@@ -17,9 +31,39 @@ export class AppController {
         return this.authService.login(req.user);
     }
 
-    @Get("portfolio.txt")
-    getFile(): StreamableFile {
-        const file = createReadStream(join(process.cwd(), "package.json"));
-        return new StreamableFile(file);
+    @Get("cv")
+    getCVRelativeUrl() {
+        const dir = join(process.cwd(), "media", "cv");
+        try {
+            const { file } = fs
+                .readdirSync(dir)
+                .filter(file => fs.lstatSync(join(dir, file)).isFile())
+                .map(file => ({ file, mtime: fs.lstatSync(join(dir, file)).mtime }))
+                .reduce((accumulator, currentValue) => {
+                    if (currentValue.mtime.getTime() > accumulator.mtime.getTime()) {
+                        return currentValue;
+                    }
+                    return accumulator;
+                });
+
+            return { relativeUrl: `/media/cv/${file}` };
+        } catch (e) {
+            throw new InternalServerErrorException("CV file is currently not available.");
+        }
+    }
+
+    @Post("cv")
+    @UseGuards(JwtAuthGuard)
+    @UseInterceptors(
+        FileInterceptor("file", {
+            storage: diskStorage({
+                destination: "./media/cv",
+                filename: (_req, _file, callback) => callback(null, "Max_Kayander_CV.pdf"),
+            }),
+            fileFilter: genericFileFilter(/^.*\.(pdf|PDF)$/),
+        })
+    )
+    postFile(@Res() res: Response, @UploadedFile() file: Express.Multer.File) {
+        return res.status(200).send({ path: file.path });
     }
 }
