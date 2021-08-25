3ecc75d21be6949ced10d93793163d663c61c134
Max Kayander <maxim.kayander1@gmail.com>
2021-08-25 10:16:23 +0300

    Added google font link.

--- src/client/pages/_document.tsx
+++ src/client/pages/_document.tsx
@@ -36,6 +36,11 @@ export default class MyDocument extends Document {
                     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
                     <link rel="icon" type="image/png" href="/favicon.png" />
                     <meta name="theme-color" content="#457b9d" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
+                        rel="stylesheet"
+                    />
                 </Head>
                 <body>
                     <Main />
