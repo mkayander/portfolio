012dccd4484a45d86b28f8c615038fa7338a39b3
Max Kayander <maxim.kayander1@gmail.com>
2021-08-26 14:08:09 +0300

    Fixed Header component test.

--- src/client/components/Header/Header.test.tsx
+++ src/client/components/Header/Header.test.tsx
@@ -1,9 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
+import { Provider } from "react-redux";
+import createConfiguredStore from "../../store";
 
-test("renders link", () => {
-    render(<Header />);
-    const linkElement = screen.getByText(/Начало/i);
-    expect(linkElement).toBeInTheDocument();
+describe("Header", () => {
+    const ReduxProvider = ({ children, reduxStore }) => <Provider store={reduxStore}>{children}</Provider>;
+    const store = createConfiguredStore();
+    const wrapper = ({ children }) => <ReduxProvider reduxStore={store}>{children}</ReduxProvider>;
+
+    beforeAll(() => {
+        process.env.NODE_ENV = "test";
+    });
+
+    test("renders link", () => {
+        render(<Header />, { wrapper });
+        const linkElement = screen.getByText(/Начало/i);
+        expect(linkElement).toBeInTheDocument();
+    });
 });
