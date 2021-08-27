9fe0b027409474bdee52af0c2e333a99eaf669a3
mkayander <maxim.kayander1@gmail.com>
2021-08-27 15:31:28 +0300

    Get protocol value from env variable.

--- src/client/utils/config.ts
+++ src/client/utils/config.ts
@@ -2,7 +2,7 @@ import isProd from "./isProd";
 
 const hostname: string | undefined = isProd ? process.env.NEXT_PUBLIC_HOSTNAME : "localhost";
 const port: string | undefined = isProd ? process.env.NEXT_PUBLIC_PORT : "3000";
-const protocol = "http";
+const protocol = process.env.NEXT_PUBLIC_PROTOCOL || "http";
 const baseUrl = `${protocol}://${hostname}${port && `:${port}`}`;
 
 export default { hostname, port, protocol, baseUrl } as const;
