61f56a77ee7bbbf00b2a6bde9529901ad08797ba
Max Kayander <maxim.kayander1@gmail.com>
2021-09-07 16:50:02 +0300

    Added blank cv info reducer.

---
+++ src/client/reducers/cvInfoReducer.ts
@@ -0,0 +1,20 @@
+import React from "react";
+
+const initialState = {};
+
+const cvInfoReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+/*
+    Actions
+*/
+
+/*
+    Selectors
+*/
+
+export default cvInfoReducer;
