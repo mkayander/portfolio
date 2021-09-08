1f5a2c14d7ad11e37df31762b23f64738a3249ba
Max Kayander <maxim.kayander1@gmail.com>
2021-09-08 23:20:42 +0300

    Apply `redux-logger` and `redux-thunk` to the Redux store.

--- src/client/store/index.ts
+++ src/client/store/index.ts
@@ -1,13 +1,16 @@
-import { compose, createStore, StoreEnhancer } from "redux";
+import { applyMiddleware, compose, createStore, StoreEnhancer } from "redux";
 import rootReducer from "../reducers";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import landingNavigation from "../landingNavigation";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 const enhancers: StoreEnhancer[] = [];
 
 enhancers.push(devToolsEnhancer({ name: "MainDevTool" }));
 
-const createConfiguredStore = (preloadedState = {}) => createStore(rootReducer, preloadedState, compose(...enhancers));
+const createConfiguredStore = (preloadedState = {}) =>
+    createStore(rootReducer, preloadedState, compose(applyMiddleware(logger, thunk), ...enhancers));
 
 const store = createConfiguredStore({ sections: landingNavigation });
 
